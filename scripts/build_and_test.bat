58d8baef2a6de450afd5d518bda3c6c25de66c04
Johan Sköld <johan@skold.cc>
2016-06-24 08:52:01 +0200

    Updating build_and_test.bat for error handling

--- scripts/build_and_test.bat
+++ scripts/build_and_test.bat
@@ -1,5 +1,14 @@
 @echo on
 
-if "%TOOLSET%" == "vs2010" if "%2" == "x64" call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" "/%1" "/%2"
+if "%TOOLSET%" == "vs2010" (
+    if "%2" == "x64" (
+        call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" "/%1" "/%2"
+        if %errorlevel% neq 0 exit /b %errorlevel%
+    )
+)
+
 msbuild /m "/p:Configuration=%1" "/p:Platform=%2" "build\%TOOLSET%\sc.sln" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 bin\sc_tests.exe --reporter console
+if %errorlevel% neq 0 exit /b %errorlevel%
