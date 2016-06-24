3c0a4764eca920f4af8f35b3d614c8d3799b9379
Johan Sköld <johan@skold.cc>
2016-06-24 12:28:57 +0200

    Disabling x64 in VS2010 for AppVeyor.
    It's misconfigured on their build workers...

--- scripts/build_and_test.bat
+++ scripts/build_and_test.bat
@@ -1,8 +1,6 @@
 @echo on
 
-if "%TOOLSET%" == "vs2010" if "%2" == "x64" call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" "/%1" "/%2"
-
-msbuild /m /p:TrackFileAccess=false "/p:Configuration=%1" "/p:Platform=%2" "build\%TOOLSET%\sc.sln" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild /m "/p:Configuration=%1" "/p:Platform=%2" "build\%TOOLSET%\sc.sln" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 bin\sc_tests.exe --reporter console
