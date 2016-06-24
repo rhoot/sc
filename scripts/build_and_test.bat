fadb7eb69d23c011b098031f6d63bcbbd1aba929
Johan Sköld <johan@skold.cc>
2016-06-24 12:02:16 +0200

    Workaround for VS2010 bug

--- scripts/build_and_test.bat
+++ scripts/build_and_test.bat
@@ -7,7 +7,7 @@ if "%TOOLSET%" == "vs2010" (
     )
 )
 
-msbuild /m "/p:Configuration=%1" "/p:Platform=%2" "build\%TOOLSET%\sc.sln" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild /m /p:TrackFileAccess=false "/p:Configuration=%1" "/p:Platform=%2" "build\%TOOLSET%\sc.sln" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 bin\sc_tests.exe --reporter console
