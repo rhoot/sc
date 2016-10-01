1c8316818a073381c00c5349edc5ff444eb1dd39
Johan Sköld <johan@arena.net>
2016-09-30 18:49:43 -0700

    Pre-fixing conditional inclusion macro for Windows

--- src/asm/make_masm.asm
+++ src/asm/make_masm.asm
@@ -2,11 +2,11 @@
 ; License: https://opensource.org/licenses/ISC
 ;
 
-IFDEF _WIN64
+IFDEF SC_WIN64
     INCLUDE make_x86_64_ms_pe_masm.asm
 ENDIF
 
-IFDEF _WIN32
+IFDEF SC_WIN32
     INCLUDE make_i386_ms_pe_masm.asm
 ENDIF
 
